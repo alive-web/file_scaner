916a90009534b4d02c12bb0dcf1b477f2d8bf54e
alive-web <alivelp@gmail.com>
2015-03-18 14:44:30 +0200

    record event in db

--- file_scaner.py
+++ file_scaner.py
@@ -2,18 +2,28 @@ import argparse
 import asyncore
 import pyinotify
 from datetime import datetime
-# from shutil import copyfile
+from pymongo import MongoClient
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',  type=str, required=True, help='path to scan directory')
 args = parser.parse_args()
-
 watched_dir = args.path
+
+client = MongoClient('localhost', 27017)
+db = client.file_scaner
+events = db.events
 wm = pyinotify.WatchManager()
 
-def write_log_in_db():
+def write_log_in_db(event):
     # This function will accept one argument "event" and will record in db
-    pass
+    action = {
+        'edited': datetime.now(),
+        'event': event.maskname,
+        'file': event.pathname
+    }
+    events.insert(action)
+
 
 def create_copy():
     # This function will accept one argument "path to file" and will record reserve copy file in db
@@ -21,20 +31,19 @@ def create_copy():
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
-        print datetime.now(), "Creating:", event.pathname
+        write_log_in_db(event)
         if event.dir:
             # restart loop
             pass
 
     def process_IN_DELETE(self, event):
-        # copyfile(event.pathname, event.pathname+str(datetime.now()))
-        print datetime.now(), "Removing:", event.pathname
+        write_log_in_db(event)
 
     def process_IN_CLOSE_WRITE(self, event):
-        print datetime.now(), "saving:", event.pathname
+        write_log_in_db(event)
 
     def process_IN_ATTRIB(self, event):
-        print datetime.now(), "changing permission:", event.pathname
+        write_log_in_db(event)
 
 notifier = pyinotify.AsyncNotifier(wm, EventHandler())
 wdd = wm.add_watch(watched_dir, pyinotify.ALL_EVENTS, rec=True)
