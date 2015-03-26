cf11e299c0fe2a780f6f0d29aff5c3a1c3f26cce
alive-web <alivelp@gmail.com>
2015-03-26 14:25:44 +0200

    add method get_tree

--- db.py
+++ db.py
@@ -1,8 +1,16 @@
 import os
-import datetime
 import config
-from models import Events, FileSystem
+import hashlib
+from datetime import datetime
 from mongoengine import connect
+from models import Events, FileSystem
+
+
+def write_fields(document, previous_version):
+    previous_version.update(set__has_next=True)
+    document.version = previous_version.version + 1
+    document.previous_version = previous_version
+    document.save()
 
 
 class DataBase():
@@ -17,39 +25,68 @@ class DataBase():
 
     def write_log(self, event):
         action = Events(event=event.maskname, path_name=event.pathname)
-        if hasattr(event, "src_pathname"):
-            action.src_path_name = event.src_pathname
+        this_document = FileSystem.objects(path_name=event.pathname).order_by("-version").first()
+        action.document = this_document
         action.save()
 
-    def put_file_revision(self, pathname, path):
-        test_doc = FileSystem.objects(path_name=pathname).order_by("-version").first()
-        st = os.stat(pathname)
+    def put_file_revision(self, pathname):
         if os.path.exists(pathname):
-            # and test_doc.permissions != st.st_mode
-
-            document = FileSystem(path_name=pathname, parent=path)
-            document.permissions = st.st_mode
+            st = os.stat(pathname)
+            previous_version = FileSystem.objects(path_name=pathname).order_by("-version").first()
             with open(pathname, 'rb') as this_file:
-                document.body.put(this_file)
-            versions = FileSystem.objects(path_name=pathname).distinct("version")
-            if len(versions) != 0:
-                document.version = max(versions) + 1
-            document.save()
+                md5_sum = hashlib.md5(this_file.read()).hexdigest()
+            document = FileSystem(path_name=pathname)
+            if md5_sum != previous_version.hash_sum:
+                with open(pathname, 'rb') as this_file:
+                    document.body.put(this_file)
+                document.hash_sum = md5_sum
+                document.permissions = st.st_mode
+                write_fields(document, previous_version)
+            if previous_version.permissions != st.st_mode:
+                document.permissions = st.st_mode
+                write_fields(document, previous_version)
 
-    def create_new(self, pathname, path, is_dir, *args):
+    def create_new(self, pathname, is_dir, watched_dir):
         if not FileSystem.objects(path_name=pathname):
-            document = FileSystem(path_name=pathname, parent=path, is_dir=is_dir)
+            document = FileSystem(path_name=pathname, is_dir=is_dir)
             document.permissions = os.stat(pathname).st_mode
-            if args:
-                document.date = args[0]
+            path_parent = os.path.split(pathname)[0]
+            if path_parent != watched_dir:
+                parent = FileSystem.objects(path_name=path_parent).order_by("-version").first()
+                document.parent = parent
+            document.date = datetime.fromtimestamp(os.path.getmtime(pathname))
             if not is_dir and os.path.exists(pathname):
                 with open(pathname, 'rb') as this_file:
                     document.body.put(this_file)
+                    md5_sum = hashlib.md5(this_file.read()).hexdigest()
+                document.hash_sum = md5_sum
             document.save()
 
-    def delete_file(self, pathname, path, is_dir):
-        document = FileSystem(path_name=pathname, parent=path, is_dir=is_dir, is_del=True)
-        versions = FileSystem.objects(path_name=pathname).distinct("version")
-        if len(versions) != 0:
-            document.version = max(versions) + 1
+    def delete_file(self, pathname):
+        previous_version = FileSystem.objects(path_name=pathname).order_by("-version").first()
+        document = FileSystem(path_name=pathname, is_del=True)
+        write_fields(document, previous_version)
+
+    def move(self, pathname, watched_dir, src_pathname=None):
+        document = FileSystem(path_name=pathname)
+        path_parent = os.path.split(pathname)[0]
+        document.permissions = os.stat(pathname).st_mode
+        if watched_dir != path_parent:
+            document.parent = FileSystem.objects(path_name=path_parent).order_by("-version").first()
+        if src_pathname:
+            previous_version = FileSystem.objects(path_name=src_pathname).order_by("-version").first()
+            previous_version.update(set__has_next=True)
+            document.version = previous_version.version + 1
+            document.previous_version = previous_version
+        check = FileSystem.objects(path_name=pathname).order_by("-version").first()
+        if check:
+            check.has_next = True
+            check.save()
         document.save()
+
+    def get_tree(self, directory):
+        path_to_file = FileSystem.objects(path_name__startswith=directory, has_next=False)
+        return path_to_file
+
+a = DataBase()
+a.get_tree("/home/plevytskyi/test_scan")
--- file_scaner.py
+++ file_scaner.py
@@ -4,6 +4,7 @@ import pyinotify
 from tree import build_tree
 from db import DataBase
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path',  type=str, required=True, help='path to scan directory')
 args = parser.parse_args()
@@ -13,7 +14,7 @@ database = DataBase()
 tree = build_tree(watched_dir)
 if tree:
     for document in tree:
-        database.create_new(document["pathname"], document["path"], document["is_dir"], document["date"])
+        database.create_new(document["pathname"], document["is_dir"], watched_dir)
 mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_ATTRIB \
        | pyinotify.IN_CLOSE_WRITE | pyinotify.IN_MOVED_TO | pyinotify.IN_MOVED_FROM
 
@@ -24,18 +25,29 @@ class EventHandler(pyinotify.ProcessEvent):
         self.db = db
 
     def process_IN_DELETE(self, event):
-        self.db.delete_file(event.pathname, event.path, event.dir)
+        self.db.delete_file(event.pathname)
+        self.db.write_log(event)
 
     def process_IN_CREATE(self, event):
-        self.db.create_new(event.pathname, event.path, event.dir)
+        self.db.create_new(event.pathname, event.dir, watched_dir)
+        self.db.write_log(event)
 
     def process_IN_CLOSE_WRITE(self, event):
-        print event
-        self.db.put_file_revision(event.pathname, event.path)
+        self.db.put_file_revision(event.pathname)
+        self.db.write_log(event)
 
     def process_IN_ATTRIB(self, event):
-        print event
-        self.db.put_file_revision(event.pathname, event.path)
+        self.db.put_file_revision(event.pathname)
+        self.db.write_log(event)
+
+    def process_IN_MOVED_TO(self, event):
+        if hasattr(event, 'src_pathname'):
+            self.db.move(event.pathname, watched_dir, event.src_pathname)
+            self.db.write_log(event)
+        else:
+            self.db.move(event.pathname, watched_dir)
+            self.db.write_log(event)
+
 
     # def process_default(self, event):
     #     print event
